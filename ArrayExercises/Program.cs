7c2b4d5ea38bdb2be6aa9479e43d63784b3a7d32
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2021-11-19 15:21:58 -0600

    Completed arrays section

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,29 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            // create and initialize an array with the values 1, 1, 2, 3, 5, 8
+            int[] integerArray = { 1, 1, 2, 3, 5, 8 };
+
+            // print out all the values in the array with a loop
+            foreach (int integer in integerArray)
+            {
+                Console.WriteLine(integer);
+            }
+
+            // create a new loop that only prints the odd integers in array
+            // ie 1, 1, 3, and 5 NOT 2 or 8
+            // how did we check even/odd integers in javascript?
+            // modulus operator: %
+            // if integer % 2 is not 0, then the integer is odd
+            // if integer is odd, print it
+            foreach (int integer in integerArray)
+            {
+                if(integer % 2 != 0)
+                {
+                    Console.WriteLine(integer);
+                }
+            }
+
         }
     }
 }
