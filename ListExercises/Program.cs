355ed51f542d7e962f287ad0b971a94469457871
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2021-11-19 16:33:00 -0600

    Completed string and list sections

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListExercises
 {
@@ -6,7 +7,63 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Lists!");
+            // create a list of integers and add in 10 integers to it
+            // int[] arrayName = etc....
+            List<int> integerList = new List<int>();
+            integerList.Add(1);
+            integerList.Add(2);
+            integerList.Add(3);
+            integerList.Add(4);
+            integerList.Add(5);
+            integerList.Add(6);
+            integerList.Add(42);
+            integerList.Add(43);
+            integerList.Add(103);
+            integerList.Add(500);
+
+            // invoke the SumEvens method and pass it our integerList as an argument
+            // and print it to the console
+            int evenSum = SumEvens(integerList);
+            Console.WriteLine(evenSum);
+
+            List<string> stringList = new List<string>();
+            stringList.Add("Pearce");
+            stringList.Add("Susan");
+            stringList.Add("Pri");
+
+            printXLetterWords(stringList, 3);
+        }
+
+        // create a static method that returns the sum of all the even numbers in a list of integers
+        // input: list of integers
+        // output: sum of even integers in list
+        public static int SumEvens(List<int> intList) 
+        {
+            int sum = 0;
+            // now iterate through intList, and add each even integer to our sum
+            foreach (int integer in intList)
+            {
+                // determine if integer is even
+                // if it is even, add it on to the sum, if it's odd, don't do anything
+                if(integer % 2 == 0)
+                {
+                    sum += integer;
+                }
+            }
+            return sum;
+        }
+
+        // method takes a list of strings and a length value x
+        // prints all words from the string list that are x letters long
+        public static void printXLetterWords(List<string> wordList, int x)
+        {
+            foreach (string word in wordList)
+            {
+                if (word.Length == x)
+                {
+                    Console.WriteLine(word);
+                }
+            }
         }
     }
 }
--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,7 +6,22 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            string rhyme = "I would not, could not, in a box. I would not, could not with a fox. " +
+                "I will not eat them in a house. I will not eat them with a mouse.";
+
+            // Use the Split method to divide the string at each space and
+            // store the individual words in an array
+            // rhyme.Split(" ") returns an array like this: ["I", "would", "not,", "could", etc...]
+            // store return in an array of strings variable
+            string[] wordsArray = rhyme.Split(" ");
+            Console.WriteLine(string.Join(",", wordsArray));
+            // repeat line 16 and 17 except split the rhyme string into an array that contains each sentence
+            string[] sentenceArray = rhyme.Split(".");
+            /*Console.WriteLine(string.Join("/", sentenceArray));*/
+            foreach (string sentence in sentenceArray)
+            {
+                Console.WriteLine(sentence);
+            }
         }
     }
 }
